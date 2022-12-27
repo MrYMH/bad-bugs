76a9918c517e5bc153f57da960885af8aa83d1db
MrYMH <https://github.com/MrYMH>
2022-12-27 22:18:33 +0200

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import motionIcon4 from "../photo/icon4.png"
+import motionIcon3 from "../photo/icon3.png"
+import motionIcon2 from "../photo/icon2.png"
+import motionIcon1 from "../photo/icon1.png"
+import {Routes, Route, useNavigate} from 'react-router-dom';
 
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//fa-5x
-import { faPenFancy  , faTv , faSquarePollHorizontal } from '@fortawesome/free-solid-svg-icons'
-// fa-square-poll-horizontal
-class Services extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <React.Fragment>
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPenFancy  , faTv , faSquarePollHorizontal , faVideo} from '@fortawesome/free-solid-svg-icons'
+
+
+const Services = () => {
+    const navigate = useNavigate();
+    const navigateCategory= () => {
+        // 👇️ navigate to /
+        navigate('/bad-bugs/categories');
+      };
+    return (
+        <React.Fragment>
                 <div id="service" className='features text-center pt-5 pb-5'>
                     <div className="container">
                         <div className="main-title mt-5 mb-5 position-relative">
@@ -17,11 +25,13 @@ class Services extends Component {
                             <p className="text-uppercase text-black-50">Some Of These Stuff Under</p>
                         </div>
                         <div>
-                            <div className="row d-flex justify-content-center">
-                                <div className="card-item col-sm-6 col-md-4 ">
+                            <div className=" row d-flex justify-content-center align-content-center">
+                                <div className="card-item col-md-3 col-sm-6 col-12 " id="card" onClick={navigateCategory} >
                                     <div className="pt-3 pb-5">
                                         <div className="icons mt-5 mb-5">
-                                            <FontAwesomeIcon className='icon fa-5x' icon={ faPenFancy} />
+                                            {/* <FontAwesomeIcon className='icon fa-5x' icon={ faPenFancy} /> */}
+                                            <img src={motionIcon1} className='icon-x fa-5x' alt="" />
+
                                         </div>
                                         <h4 className="text-uppercase">Graphic Design</h4>
                                         <p>
@@ -29,10 +39,12 @@ class Services extends Component {
                                         </p>
                                     </div>
                                 </div>
-                                <div className="card-item col-sm-6 col-md-4 ">
+                                <div className="card-item col-md-3 col-sm-6 col-12 " id="card">
                                     <div className="pt-3 pb-5">
                                         <div className="icons mt-5 mb-5">
-                                            <FontAwesomeIcon className='icon fa-5x' icon={ faTv} />
+                                            {/* <FontAwesomeIcon className='icon fa-5x' icon={ faTv} /> */}
+                                            <img src={motionIcon2} className='icon-x fa-5x' alt="" />
+
                                         </div>
                                         <h4 className="text-uppercase">Web Development</h4>
                                         <p>
@@ -40,24 +52,38 @@ class Services extends Component {
                                         </p>
                                     </div>
                                 </div>
-                                <div className="card-item col-sm-6 col-md-4 ">
+                                <div className="card-item col-md-3 col-sm-6 col-12 " id="card">
                                     <div className="pt-3 pb-5">
                                         <div className="icons mt-5 mb-5">
-                                            <FontAwesomeIcon className='icon fa-5x' icon={ faSquarePollHorizontal} />
+                                            {/* <FontAwesomeIcon className='icon fa-5x' icon={ faVideo} /> */}
+                                            <img src={motionIcon3} className='icon-x fa-5x' alt="" />
+
                                         </div>
-                                        <h4 className="text-uppercase">Content Creation</h4>
+                                        <h4 className="text-uppercase">video creation</h4>
                                         <p>
                                             Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
                                         </p>
                                     </div>
                                 </div>
+                                <div className="card-item col-md-3 col-sm-6 col-12  " id="card">
+                                    <div className="pt-3 pb-5">
+                                        <div className="icons mt-5 mb-5">
+                                            {/* <FontAwesomeIcon className='icon fa-5x' icon={ faSquarePollHorizontal} /> */}
+                                            <img src={motionIcon4} className='icon-x fa-5x' alt="" />
+                                        </div>
+                                        <h4 className="text-uppercase">motion Graphic</h4>
+                                        <p>
+                                            Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                                        </p>
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
                 </div>
             </React.Fragment>
-        );
-    }
+    );
 }
- 
+
 export default Services;
